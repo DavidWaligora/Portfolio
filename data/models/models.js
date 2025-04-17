2786fb206ec13f3af8f44237fbad58186e6ca10a
r0989841 <r0989841@student.thomasmore.be>
2025-04-17 22:08:29 +0200

    first commit

---
+++ data/models/models.js
@@ -0,0 +1,12 @@
+class Education {
+    constructor(school, degree, date) {
+      this.school = school;
+      this.degree = degree;
+      this.date = date;
+    }
+}
+class Skill {
+  constructor(title, items) {
+    this.title = title;
+    this.items = items;
+  }
